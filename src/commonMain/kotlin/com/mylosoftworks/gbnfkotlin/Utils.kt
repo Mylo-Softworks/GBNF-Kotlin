a1eeb2dd1e73d13b5ee35aa661e86765a4afd722
Mylo <36931363+gitmylo@users.noreply.github.com>
2024-12-17 15:23:19 +0100

    Update sanitizing

--- src/commonMain/kotlin/com/mylosoftworks/gbnfkotlin/Utils.kt
+++ src/commonMain/kotlin/com/mylosoftworks/gbnfkotlin/Utils.kt
@@ -4,7 +4,7 @@ package com.mylosoftworks.com.mylosoftworks.gbnfkotlin
  * Sanitize a string for literal in GBNF
  */
 fun sanitizeGBNFString(input: String): String {
-    return input
-        .replace("\"", "\\").replace("\n", "\\n")
-        .replace("\t", "\\t").replace("\\", "\\\\")
+    return input.replace("\\", "\\\\")
+        .replace("\"", "\\\"").replace("\'", "\\'").replace("`", "\\`")
+        .replace("\n", "\\n").replace("\t", "\\t").replace("\r", "\\r")
 }
