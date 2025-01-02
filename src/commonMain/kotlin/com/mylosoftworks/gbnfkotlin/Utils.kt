eac915f76a254657e77e3702e12f70f4086ff916
Mylo <36931363+gitmylo@users.noreply.github.com>
2025-01-02 16:14:25 +0100

    Remove broken escapes

--- src/commonMain/kotlin/com/mylosoftworks/gbnfkotlin/Utils.kt
+++ src/commonMain/kotlin/com/mylosoftworks/gbnfkotlin/Utils.kt
@@ -4,7 +4,6 @@ package com.mylosoftworks.gbnfkotlin
  * Sanitize a string for literal in GBNF
  */
 fun sanitizeGBNFString(input: String): String {
-    return input.replace("\\", "\\\\")
-        .replace("\"", "\\\"").replace("\'", "\\'").replace("`", "\\`")
+    return input.replace("\\", "\\\\").replace("\"", "\\\"")
         .replace("\n", "\\n").replace("\t", "\\t").replace("\r", "\\r")
 }
